890bb13c7b9ef25675b78cf9ef5398ea63ed4efc
Amir B. Kahna <amir.kahna2@gmail.com>
2026-07-13 15:44:41 +0200

    Initial commit

---
+++ src/main/java/amirka/u5w3d1/U5w3d1Application.java
@@ -0,0 +1,13 @@
+package amirka.u5w3d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w3d1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w3d1Application.class, args);
+	}
+
+}
---
+++ src/test/java/amirka/u5w3d1/U5w3d1ApplicationTests.java
@@ -0,0 +1,13 @@
+package amirka.u5w3d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w3d1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
